8236f01d273a81f7d54ed09ae4e534688a794d52
Reon Kovalchuk <thesmollestlion@gmail.com>
2023-12-17 15:10:04 +0400

    начало "игры" с падающими звездочками

---
+++ app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="stars__container">
+    <Star
+      v-for="star in stars"
+      :key="star.id"
+      :style="`left:${star.left};animation-duration:${star.animationDuration}; font-size:${star.fontSize}`"
+    />
+  </div>
+</template>
+<script setup>
+const starsRate = 500;
+const stars = ref([]);
+function createStar() {
+  stars.value.push({
+    id: new Date().valueOf(),
+    left: Math.random() * +innerWidth + "px",
+    animationDuration: 4 + Math.random() * 4 + "s",
+    fontSize: 20 + Math.random() * 30 + "px",
+  });
+}
+
+setTimeout(() => {
+  stars.value.shift();
+  setInterval(() => {
+    stars.value.shift();
+  }, starsRate);
+}, 6000);
+
+setInterval(() => {
+  createStar();
+}, starsRate);
+</script>
---
+++ components/Star.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="star"></div>
+</template>
+<style lang="scss" scoped>
+$size: random() * 12;
+$duration: random() * 3;
+$container-width: 1600;
+.star {
+  left: random() * $container-width + "px";
+  font-size: 12 + $size + "px";
+  animation-duration: 4 + $duration + "s";
+}
+</style>
